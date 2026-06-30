3ef3db19c0b8d16c71efaa712355b93fe326bf51
Rocky7842 <eric.rocky7842@gmail.com>
2026-06-30 15:58:35 +0800

    sm8250-common: Enable SecureNFC
    * Should be supported by all NFC targets.
    
    Change-Id: Ib1bfb9df26ab9d65d01c0b84ff338353a85f62f3

--- kona.mk
+++ kona.mk
@@ -269,6 +269,7 @@ $(call soong_config_set_bool,stagefright,target_disable_thumbnail_block_model,tr
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp \
     com.android.nfc_extras \
+    NfcResCommon \
     Tag
 
 PRODUCT_COPY_FILES += \
